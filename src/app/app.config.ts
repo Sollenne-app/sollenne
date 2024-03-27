d016ecbd2864a82f9940e32f5c25c2bcbe148d3e
Rich Sollenne <rich.sollenne@improving.com>
2024-03-27 14:37:48 -0500

    add firebase hosting

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,30 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getAnalytics, provideAnalytics } from '@angular/fire/analytics';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideClientHydration(), provideRouter(appRoutes)],
+  providers: [
+    provideClientHydration(),
+    provideRouter(appRoutes),
+    importProvidersFrom([
+      provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+      provideFirestore(() => getFirestore()),
+      provideAnalytics(() => getAnalytics()),
+      provideAuth(() => getAuth()),
+      provideFunctions(() => getFunctions()),
+      provideMessaging(() => getMessaging()),
+      providePerformance(() => getPerformance()),
+      provideStorage(() => getStorage()),
+    ])
+  ],
 };
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+
+  firebaseConfig: {
+    apiKey: 'AIzaSyA0H-x8U2k0x8ylfdChILsQhC5-fZLPQZI',
+    authDomain: 'sollenne-app.firebaseapp.com',
+    projectId: 'sollenne-app',
+    storageBucket: 'sollenne-app.appspot.com',
+    messagingSenderId: '681523002774',
+    appId: '1:681523002774:web:83c7311e0db1fa0e0131b7',
+    measurementId: 'G-GL6LBN2GWS',
+  },
+
+};
